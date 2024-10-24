5067298d6d7da4cc74f9b4a7fc5b4a68895c0b8f
NanditoMaulanaYedikar <yedikarmaulananandito@gmail.com>
2024-10-24 12:56:35 +0700

    first commit

---
+++ page/beranda.php
@@ -0,0 +1,405 @@
+ <!-- Hero Section -->
+ <section id="hero" class="hero section light-background">
+
+<div class="container">
+  <div class="row gy-4 justify-content-center justify-content-lg-between">
+    <div class="col-lg-5 order-2 order-lg-1 d-flex flex-column justify-content-center">
+      <h1 data-aos="fade-up">Foodloop<br>Delicious Food</h1>
+      <p data-aos="fade-up" data-aos-delay="100">Kami menyediakan makanan berkualitas dengan harga yang murah.</p>
+      <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
+        <a href="#book-a-table" class="btn-get-started">Contact</a>
+      </div>
+    </div>
+    <div class="col-lg-5 order-1 order-lg-2 hero-img" data-aos="zoom-out">
+      <img src="assets/img/logofood.png" class="img-fluid animated" alt="">
+    </div>
+  </div>
+</div>
+
+</section><!-- /Hero Section -->
+
+<!-- About Section -->
+<section id="about" class="about section">
+
+<!-- Section Title -->
+<div class="container section-title" data-aos="fade-up">
+  <p><span>Learn More</span> <span class="description-title" style="color: #097D4C;">About Us</span></p>
+</div><!-- End Section Title -->
+
+<div class="container">
+
+  <div class="row gy-4">
+    <div class="col-lg-7" data-aos="fade-up" data-aos-delay="100">
+      <img src="assets/img/foodloop3.png"  class="img-fluid mb-4" alt="">
+    </div>
+    <div class="col-lg-5" data-aos="fade-up" data-aos-delay="250">
+      <div class="content ps-0 ps-lg-5">
+        <p class="fst-italic">
+          Kerjasama dengan restoran untuk aplikasi Foodloop meliputi penyusunan kontrak kemitraan yang jelas, pemilihan restoran dengan reputasi baik, serta pemberian akses untuk mengelola makanan dan pelatihan staf. 
+        </p>
+        <p style="font-weight: 500;">Visi : </p>
+        <ul>
+          <li style="font-weight: 300;"><span>Mengurangi jumlah makanan yang terbuang dari restauran dan supermarket dengan menjual makanan yang hampir kedaluwarsa atau hampir tidak terjual.</span></li>
+        <ul>
+        <p style="font-weight: 500;">Misi : </p>
+        <ul>
+          <li style="font-weight: 300;"><i class="bi bi-check-circle-fill" style="color: #097D4C;"></i> <p></p><span>Memungkinkan konsumen membeli makanan sisa berkualitas tinggi dengan harga terjangkau.</span></li>
+          <li style="font-weight: 300;"><i class="bi bi-check-circle-fill" style="color: #097D4C;"></i> <span>Mendukung restoran dalam mengurangi limbah makanan dan meningkatkan efisiensi.</span></li>
+          <li style="font-weight: 300;"><i class="bi bi-check-circle-fill" style="color: #097D4C;"></i> <span>Meningkatkan kesadaran tentang pentingnya mengurangi limbah makanan.</span></li>
+          <li style="font-weight: 300;"><i class="bi bi-check-circle-fill" style="color: #097D4C;"></i> <span>Memberikan solusi inovatif dan ramah lingkungan melalui teknologi yang mudah digunakan.</span></li>
+
+        </ul>
+      
+
+        <div class="position-relative mt-4">
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+</section><!-- /About Section -->
+
+<!-- Why Us Section -->
+<section id="why-us" class="why-us section light-background">
+
+<div class="container">
+
+  <div class="row gy-4">
+
+    <div class="col-lg-4" data-aos="fade-up" data-aos-delay="100">
+      <div class="why-box">
+        <h3>Why Choose Foodloop</h3>
+        <p>
+          Pilih Foodloop untuk makanan sisa berkualitas tinggi dengan harga terjangkau dari restoran terpercaya. Bantu mengurangi limbah makanan, dukung bisnis lokal, dan nikmati hidangan segar melalui aplikasi yang mudah digunakan, sambil memberikan dampak positif pada lingkungan.
+        </p>
+        <div class="text-center">
+          <a href="#" class="more-btn"><span>Learn More</span> <i class="bi bi-chevron-right"></i></a>
+        </div>
+      </div>
+    </div><!-- End Why Box -->
+
+    <div class="col-lg-8 d-flex align-items-stretch">
+      <div class="row gy-4" data-aos="fade-up" data-aos-delay="200">
+
+        <div class="col-xl-4">
+          <div class="icon-box d-flex flex-column justify-content-center align-items-center">
+            <h4>Mengurangi limbah makanan</h4>
+            <p>Dengan menjual makanan yang sudah waktunya tidak bisa di jual pada Resto , Foodloop membantu mengurangi pemborosan makanan dan mendukung keberlanjutan lingkungan.</p>
+          </div>
+        </div><!-- End Icon Box -->
+
+        <div class="col-xl-4" data-aos="fade-up" data-aos-delay="300">
+          <div class="icon-box d-flex flex-column justify-content-center align-items-center">
+            <h4>Kualitas Terjangkau</h4>
+            <p>Makanan sisa berkualitas tinggi dengan harga diskon. Konsumen dapat membeli makanan dengan harga diskon, yang menarik bagi mereka yang ingin menghemat pengeluaran.</p>
+          </div>
+        </div><!-- End Icon Box -->
+
+        <div class="col-xl-4" data-aos="fade-up" data-aos-delay="400">
+          <div class="icon-box d-flex flex-column justify-content-center align-items-center">
+            <h4>Memberi dampak sosial positif</h4>
+            <p>Aplikasi ini mendukung gerakan anti-limbah dan menciptakan kesadaran lebih luas tentang pentingnya mengelola sumber daya dengan bijak.</p>
+          </div>
+        </div><!-- End Icon Box -->
+
+      </div>
+    </div>
+
+  </div>
+
+</div>
+
+</section><!-- /Why Us Section -->
+
+
+<!-- Menu Section -->
+<section id="menu" class="menu section">
+
+<!-- Section Title -->
+<div class="container section-title" data-aos="fade-up">
+  <h2>Our Menu</h2>
+  <p><span>Check Our</span> <span class="description-title">Foodloop Menu</span></p>
+</div><!-- End Section Title -->
+
+<div class="container">
+
+  <ul class="nav nav-tabs d-flex justify-content-center" data-aos="fade-up" data-aos-delay="100">
+
+    <li class="nav-item">
+      <a class="nav-link active show" data-bs-toggle="tab" data-bs-target="#menu-starters">
+        <h4>Makanan</h4>
+      </a>
+    </li><!-- End tab nav item -->
+
+    <li class="nav-item">
+      <a class="nav-link" data-bs-toggle="tab" data-bs-target="#menu-breakfast">
+        <h4>Minuman</h4>
+      </a><!-- End tab nav item -->
+
+    </li>
+
+
+  </ul>
+
+  <div class="tab-content" data-aos="fade-up" data-aos-delay="200">
+
+    <div class="tab-pane fade active show" id="menu-starters">
+
+      <div class="tab-header text-center">
+        <p>Menu</p>
+        <h3>Makanan</h3>
+      </div>
+
+      <div class="row gy-5">
+
+        <div class="col-lg-4 menu-item">
+          <a href="assets/img/m1.jpg" class="glightbox"><img src="assets/img/m1.jpg" class="menu-img img-fluid" alt=""></a>
+          <h4>Brownies</h4>
+          <p class="ingredients">
+            Restaurant Brownies Buhajah
+          </p>
+          <p class="price">
+            Rp. 4.000
+          </p>
+        </div><!-- Menu Item -->
+
+        <div class="col-lg-4 menu-item">
+          <a href="assets/img/m2.jpg" class="glightbox"><img src="assets/img/m2.jpg" class="menu-img img-fluid" alt=""></a>
+          <h4>Croissant</h4>
+          <p class="ingredients">
+            Restaurant Tambun Pastri
+          </p>
+          <p class="price">
+            Rp. 5.499
+          </p>
+        </div><!-- Menu Item -->
+
+        <div class="col-lg-4 menu-item">
+          <a href="assets/img/m3.jpg" class="glightbox"><img src="assets/img/m3.jpg" class="menu-img img-fluid" alt=""></a>
+          <h4>Donut</h4>
+          <p class="ingredients">
+            Restaurant donHill
+          </p>
+          <p class="price">
+            Rp. 3.500
+          </p>
+        </div><!-- Menu Item -->
+
+        <div class="col-lg-4 menu-item">
+          <a href="assets/img/m4.jpg" class="glightbox"><img src="assets/img/m4.jpg" class="menu-img img-fluid" alt=""></a>
+          <h4>Roti</h4>
+          <p class="ingredients">
+            Restaurant Roti Paogah
+          </p>
+          <p class="price">
+            Rp. 4.765
+          </p>
+        </div><!-- Menu Item -->
+
+        <div class="col-lg-4 menu-item">
+          <a href="assets/img/m5.jpg" class="glightbox"><img src="assets/img/m5.jpg" class="menu-img img-fluid" alt=""></a>
+          <h4>Cookies</h4>
+          <p class="ingredients">
+            Restaurant Haji Cookies
+          </p>
+          <p class="price">
+            Rp. 5.999
+          </p>
+        </div><!-- Menu Item -->
+
+        <div class="col-lg-4 menu-item">
+          <a href="assets/img/m6.jpg" class="glightbox"><img src="assets/img/m6.jpg" class="menu-img img-fluid" alt=""></a>
+          <h4>Pizza</h4>
+          <p class="ingredients">
+            Restaurant Pizza Mario
+          </p>
+          <p class="price">
+            Rp. 11.676
+          </p>
+        </div><!-- Menu Item -->
+
+      </div>
+    </div><!-- End Starter Menu Content -->
+
+    <div class="tab-pane fade" id="menu-breakfast">
+
+      <div class="tab-header text-center">
+        <p>Menu</p>
+        <h3>Minuman</h3>
+      </div>
+
+      <div class="row gy-5">
+
+        <div class="col-lg-4 menu-item">
+          <a href="assets/img/j1.jpg" class="glightbox"><img src="assets/img/j1.jpg" class="menu-img img-fluid" alt=""></a>
+          <h4>Thai tea</h4>
+          <p class="ingredients">
+            Cafe Teras Kopi
+          </p>
+          <p class="price">
+            Rp. 9.879
+          </p>
+        </div><!-- Menu Item -->
+
+        <div class="col-lg-4 menu-item">
+          <a href="assets/img/j2.jpg" class="glightbox"><img src="assets/img/j2.jpg" class="menu-img img-fluid" alt=""></a>
+          <h4>Matchalatte</h4>
+          <p class="ingredients">
+            Cafe Kopi Satu Detik
+          </p>
+          <p class="price">
+            Rp. 11.700
+          </p>
+        </div><!-- Menu Item -->
+
+        <div class="col-lg-4 menu-item">
+          <a href="assets/img/j3.jpg" class="glightbox"><img src="assets/img/j3.jpg" class="menu-img img-fluid" alt=""></a>
+          <h4>Smoothies</h4>
+          <p class="ingredients">
+            Cafe Coffe Rooftop
+          </p>
+          <p class="price">
+            Rp. 13.200
+          </p>
+        </div><!-- Menu Item -->
+
+        <div class="col-lg-4 menu-item">
+          <a href="assets/img/j4.jpg" class="glightbox"><img src="assets/img/j4.jpg" class="menu-img img-fluid" alt=""></a>
+          <h4>Coffee</h4>
+          <p class="ingredients">
+            Cafe Trata
+          </p>
+          <p class="price">
+            Rp. 12.000
+          </p>
+        </div><!-- Menu Item -->
+
+        <div class="col-lg-4 menu-item">
+          <a href="assets/img/j5.jpg" class="glightbox"><img src="assets/img/j5.jpg" class="menu-img img-fluid" alt=""></a>
+          <h4>Infused Water</h4>
+          <p class="ingredients">
+            Cafe Tatanapan
+          </p>
+          <p class="price">
+            Rp. 14.700
+          </p>
+        </div><!-- Menu Item -->
+
+        <div class="col-lg-4 menu-item">
+          <a href="assets/img/j6.jpg" class="glightbox"><img src="assets/img/j6.jpg" class="menu-img img-fluid" alt=""></a>
+          <h4>Milk Tea</h4>
+          <p class="ingredients">
+            Cafe Rindu
+          </p>
+          <p class="price">
+            Rp. 9.600
+          </p>
+        </div><!-- Menu Item -->
+
+      </div>
+    </div><!-- End Breakfast Menu Content -->
+
+
+
+  </div>
+
+</div>
+
+</section><!-- /Menu Section -->
+
+
+
+
+
+
+
+<!-- Contact Section -->
+<section id="contact" class="contact section">
+
+<!-- Section Title -->
+<div class="container section-title" data-aos="fade-up">
+  <h2>Contact</h2>
+  <p><span>Need Help?</span> <span class="description-title">Contact Us</span></p>
+</div><!-- End Section Title -->
+
+  <div class="row gy-4">
+
+    <div class="col-md-6">
+      <div class="info-item d-flex align-items-center" data-aos="fade-up" data-aos-delay="200">
+        <i class="icon bi bi-geo-alt flex-shrink-0"></i>
+        <div>
+          <h3>Address</h3>
+          <p>Jalan Makmur No. 45, Jakarta Selatan, DKI Jakarta 12345
+          </p>
+        </div>
+      </div>
+    </div><!-- End Info Item -->
+
+    <div class="col-md-6">
+      <div class="info-item d-flex align-items-center" data-aos="fade-up" data-aos-delay="300">
+        <i class="icon bi bi-telephone flex-shrink-0"></i>
+        <div>
+          <h3>Call Us</h3>
+          <p>+62 85281798549</p>
+        </div>
+      </div>
+    </div><!-- End Info Item -->
+
+    <div class="col-md-6">
+      <div class="info-item d-flex align-items-center" data-aos="fade-up" data-aos-delay="400">
+        <i class="icon bi bi-envelope flex-shrink-0"></i>
+        <div>
+          <h3>Email Us</h3>
+          <p>foodloop@gmail.com
+          </p>
+        </div>
+      </div>
+    </div><!-- End Info Item -->
+
+    <div class="col-md-6">
+      <div class="info-item d-flex align-items-center" data-aos="fade-up" data-aos-delay="500">
+        <i class="icon bi bi-clock flex-shrink-0"></i>
+        <div>
+          <h3>Opening Hours<br></h3>
+          <p>Every Day</p>
+        </div>
+      </div>
+    </div><!-- End Info Item -->
+
+  </div>
+
+  <form action="forms/contact.php" method="post" class="php-email-form" data-aos="fade-up" data-aos-delay="600">
+    <div class="row gy-4">
+
+      <div class="col-md-6">
+        <input type="text" name="name" class="form-control" placeholder="Your Name" required="">
+      </div>
+
+      <div class="col-md-6 ">
+        <input type="email" class="form-control" name="email" placeholder="Your Email" required="">
+      </div>
+
+      <div class="col-md-12">
+        <input type="text" class="form-control" name="subject" placeholder="Subject" required="">
+      </div>
+
+      <div class="col-md-12">
+        <textarea class="form-control" name="message" rows="6" placeholder="Message" required=""></textarea>
+      </div>
+
+      <div class="col-md-12 text-center">
+        <div class="loading">Loading</div>
+        <div class="error-message"></div>
+        <div class="sent-message">Your message has been sent. Thank you!</div>
+
+        <button type="submit">Send Message</button>
+      </div>
+
+    </div>
+  </form><!-- End Contact Form -->
+
+</div>
+
+</section><!-- /Contact Section -->
---
+++ page/profile.php
@@ -0,0 +1,83 @@
+<style>
+    @media (max-width: 600px) {
+        ul li strong {
+            min-width: 120px;
+        }
+        ul li {
+            flex-direction: column; /* Membuat teks vertikal di layar kecil */
+            align-items: flex-start;
+        }
+        ul li span {
+            margin-left: 20px;
+        }
+    }
+
+</style>
+<html>
+    <section id="about2" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2 style="font-size: 24px;">Profile</h2>
+        </div>
+
+        <div class="row">
+        <div class="col-lg-4">
+            <img src="assets/img/abin.jpeg" class="img-fluid mb-4" alt="" style="height: 400px; width: 100%; object-fit: cover;">
+            </div>
+          <div   class="col-lg-8 pt-4 pt-lg-0 content">
+            <h3>Data Diri</h3>
+            <div class="row">
+                <div class="col-lg-12">
+                <ul style="list-style: none; padding: 0;">
+                    <li style="display: flex; align-items: flex-start;">
+                        <i class="bi bi-rounded-right" style="margin-right: 10px;"></i> 
+                        <strong style="min-width: 150px;">Nama:</strong> 
+                        Nandito maulana yedikar
+                    </li>
+                    <li style="display: flex; align-items: flex-start;">
+                        <i class="bi bi-rounded-right" style="margin-right: 10px;"></i> 
+                        <strong style="min-width: 150px;">Email:</strong> 
+                        nanditomaulanayedikar@gmail.com
+                    </li>
+                    <li style="display: flex; align-items: flex-start;">
+                        <i class="bi bi-rounded-right" style="margin-right: 10px;"></i> 
+                        <strong style="min-width: 150px;">Alamat:</strong> 
+                        JL. Melati no. 138 Kelurahan Patokan, Kecamatan Kraksaan, Jawa Timur
+                    </li>
+                    <li style="display: flex; align-items: flex-start;">
+                        <i class="bi bi-rounded-right" style="margin-right: 10px;"></i> 
+                        <strong style="min-width: 150px;">Handphone:</strong> 
+                        083119051731
+                    </li>
+                </ul>
+                </div>
+            </div>
+          </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+    
+    <section id="skills" class="skills">
+    <link rel="stylesheet" href="../assets/css/style.css">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+        <style>
+            span:hover{
+                color: #7ED957;
+            }
+            a:hover{
+                color: #7ED957;
+            }
+        </style>
+          <h3 style="font-size: 26px; font-weight: 600;">NANDITO MAULANA YEDIKAR <a style="font-weight: 900;" href=".?beranda#menu">Mau makan apa?</a></h3>
+        </div>
+        
+      </div>
+      
+    </section>
+</html>
